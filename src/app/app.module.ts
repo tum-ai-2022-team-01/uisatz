46169c428137f70393ee2cccdde5f836afaa15ad
Wenzel Pünter <wenzel@T-1000.local>
2022-10-01 19:48:14 +0200

    :sparkles: Simplify UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BpmnComponent } from './bpmn/bpmn.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BpmnComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
