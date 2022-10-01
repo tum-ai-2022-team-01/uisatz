e011d77174581e74090b23deb122a24df9b3990f
Wenzel Pünter <wenzel@T-1000.local>
2022-10-01 17:07:01 +0200

    :sparkles: Comprehensive draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BpmnComponent } from './bpmn/bpmn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BpmnComponent
   ],
   imports: [
     BrowserModule
